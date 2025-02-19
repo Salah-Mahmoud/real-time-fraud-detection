daf1f5cea71dc9604d84e73a064497069951bc6a
Salah Mahmoud <124273567+Salah-Mahmoud@users.noreply.github.com>
2025-02-19 18:57:13 +0200

    botToken

--- spark_apps/utility.py
+++ spark_apps/utility.py
@@ -1,7 +1,7 @@
 import requests
 
-BOT_TOKEN = "8048788363:AAFSByAWeGGwmYKeszrT5xnXBtOJ6FJhWxQ"
-CHAT_ID = "1152896402"
+BOT_TOKEN = "804****363:AAFSByA*********eszrT5xnXBtOJ6FJhWxQ"
+CHAT_ID = "115*****6402"
 
 def send_message(text):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
